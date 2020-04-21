7ec2d7dd3339cec673b31bed9c8a18c141312593
tiantiantaoke <yujiajun901019@126.com>
2020-04-21 18:07:11 +0800

    新习题

--- Chapter16.cpp
+++ Chapter16.cpp
@@ -91,5 +91,27 @@ void Exercise16_14()
 	
 	Screen<5, 5> nextScreen('X');
 	nextScreen.move(4, 0).set('#').display(cout);
-	cout << 
+	cout << "\n";
+	nextScreen.display(cout);
+	cout << endl;
+
+	const Screen<5, 3> blank;
+	myScreen.set('#').display(cout);
+	cout << endl;
+	blank.display(cout);
+	cout << endl;
+
+	myScreen.clear('Z').display(cout);
+	cout << endl;
+	myScreen.move(4, 0);
+	myScreen.set('#');
+	myScreen.display(cout);
+	cout << endl;
+	myScreen.clear('Z').display(cout);
+	cout << endl;
+
+	Screen<5, 3> temp = myScreen.move(4, 0);
+	temp.set('#');
+	myScreen.display(cout);
+	cout << endl;
 }
--- test.cpp
+++ test.cpp
@@ -122,7 +122,7 @@ void fun2(A& a)
 
 int main(int argc, char * argv[])
 {
-	Exercise15_36();
+	Exercise16_14();
 
 
 	system("pause");
