9d8f8969eebebe51fe64836f7abbca2aaaaae440
TianTianTaoKe <yujiajun901019@126.com>
2022-09-14 22:19:25 +0800

    使用strcpy_s  strcat_s

--- Chapter12.cpp
+++ Chapter12.cpp
@@ -198,8 +198,8 @@ char* addStr(const char* str1, const char* str2)
 	size_t strlen2 = strlen(str2);
 	size_t strlenAll = strlen1 + strlen2 + 1;
 	char * newStr = new char[strlenAll]();
-	strcpy(newStr, str1);
-	strcat(newStr, str2);
+	strcpy_s(newStr, strlenAll, str1);
+	strcat_s(newStr, strlenAll, str2);
 	return newStr;
 }
 
@@ -207,7 +207,7 @@ char* addStr(const string& str1, const string& str2)
 {
 	string newStr = str1 + str2;
 	char *newCharArr = new char[newStr.size() + 1];
-	strcpy(newCharArr, newStr.c_str());
+	strcpy_s(newCharArr, newStr.size() + 1, newStr.c_str());
 	return newCharArr;
 }
 
